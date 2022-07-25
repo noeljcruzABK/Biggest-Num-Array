7ce7a4e21da68a165db24833f8465259f991ddfe
Noel Cruz <nocruz@activision.com>
2022-07-25 09:44:22 -0400

    created populateArray and getLargest functions. printing the results.

--- Biggest-Num-Array/Biggest-Num-Array.cpp
+++ Biggest-Num-Array/Biggest-Num-Array.cpp
@@ -4,7 +4,29 @@
 #include <iostream>
 using namespace std;
 
+void populateArr(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+}
+
+int getLargest(int arr[], int size) {
+    int largest = arr[0];
+
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+    int numsArray[5] = {};
+
+    cout << "Please enter a series of 5 whole numbers:" << endl;
+    populateArr(numsArray, 5);
+
+    cout << "The largest number you entered is: " << getLargest(numsArray, 5) << "." << endl;
 }
